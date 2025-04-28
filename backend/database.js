29a24317f0a696e7eea289314ec22178472bb8f3
Prakhar17m <131417300+Prakhar17m@users.noreply.github.com>
2025-04-28 10:04:05 +0530

    first commit

---
+++ backend/database.js
@@ -0,0 +1,12 @@
+const mongoose =require('mongoose');
+async function dbConn(){
+    const conn=await mongoose.connect('mongodb+srv://testmongo:root@cluster0.r5qoqyv.mongodb.net/abes-crud?retryWrites=true&w=majority&appName=Cluster0');
+    if(conn){
+        console.log('database connect successfully')
+
+    }
+    else{
+        console.log('connection fail');
+    }
+}
+module.exports=dbConn;
