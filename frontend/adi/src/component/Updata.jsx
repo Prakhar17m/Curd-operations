976af184353d322d189b02dba27ddb7b609870fb
Prakhar17m <131417300+Prakhar17m@users.noreply.github.com>
2025-04-30 09:58:17 +0530

    Your descriptive commit message

--- frontend/adi/src/component/Updata.jsx
+++ frontend/adi/src/component/Updata.jsx
@@ -8,7 +8,7 @@ function Updata() {
         const name=e.target.name.value;
         const age =e.target.age.value;
         const data={name,age}
-        await axios.put(`http://localhost:9000/users/${id}`,data)
+        await axios.put(`https://curd-operations-1-xcp8.onrender.com/users/${id}`,data)
         alert("DATA UPDATE")
     }
   return (
